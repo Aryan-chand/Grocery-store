8f7872b80f099d73c370fdf9fbb49f98e059bdac
Aryan-chand <imaryan1803@gmail.com>
2024-04-17 21:59:57 +0530

    error fixed

--- UI/js/custom/common.js
+++ UI/js/custom/common.js
@@ -1,4 +1,3 @@
-// Define your api here
 var productListApiUrl = 'http://127.0.0.1:5000/getProducts';
 var uomListApiUrl = 'http://127.0.0.1:5000/getUOM';
 var productSaveApiUrl = 'http://127.0.0.1:5000/insertProduct';
@@ -6,7 +5,6 @@ var productDeleteApiUrl = 'http://127.0.0.1:5000/deleteProduct';
 var orderListApiUrl = 'http://127.0.0.1:5000/getAllOrders';
 var orderSaveApiUrl = 'http://127.0.0.1:5000/insertOrder';
 
-// For product drop in order
 var productsApiUrl = 'https://fakestoreapi.com/products';
 
 function callApi(method, url, data) {
